a251641a040546360c69a57126c23a97d536d8dc
Thiag0MR <thiagoomendes2010@gmail.com>
2021-07-17 11:23:37 -0300

    URI_1241

--- strings/URI_1241.java
+++ strings/URI_1241.java
@@ -1,43 +1,39 @@
 package strings;
 
+import java.io.IOException;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class URI_1241 {
-    public static void main(String[] args) {
-        
-        Scanner sc = new Scanner (System.in);
-        sc.useDelimiter("\\s");
-
-        int n = sc.nextInt();
-        sc.nextLine();
-
-        for (int i = 0; i < n; i++) {
-            String a = sc.next();
-            String b = sc.next();
-
-            if (a.length() >= b.length()) {
-
-                int j = b.length() - 1;
-
-                boolean v = false;
-                while (j >= 0) {
-                    int x = a.length() - ((b.length() - 1) - j) - 1;
-                    if (b.charAt(j) != a.charAt(x)) {
-                        System.out.println("nao encaixa");
-                        v = true;
-                        break;
-                    }
-                    j--;
-                }
-                if (!v) {
-                    System.out.println("encaixa");
-                }
-                
-
-            } else {
-                System.out.println("nao encaixa");
-            }
-        }
-        sc.close();
+	
+    public static void main(String[] args) throws IOException {
+    	Scanner leitor = new Scanner(System.in);
+    	int n = leitor.nextInt();
+    	
+    	for (int i = 0; i < n; i++) {
+    		String a = leitor.next();
+    		String b = leitor.next();
+    		
+    		if (a.length() == b.length()) {
+    			if (a.equals(b)) {
+    				System.out.println("encaixa");
+    			} else {
+    				System.out.println("nao encaixa");
+    			}
+    		} else {
+    			if (a.length() > b.length()) {
+    				if (Objects.equals(a.substring(a.length() - b.length()), b)) {
+    					System.out.println("encaixa");
+    				} else {
+    					System.out.println("nao encaixa");
+    				}
+    			} else {
+    				System.out.println("nao encaixa");
+    			}
+    		}
+    	}
+    	
+    	leitor.close();
     }
+	
 }
